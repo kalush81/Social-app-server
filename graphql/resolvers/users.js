9b604e8495067b24eff9f549b38f58579a687ad6
kalush81 <krzysiek.kaluza@gmail.com>
2021-03-28 14:18:32 +0200

    Hash password

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,13 +1,17 @@
+const bcrypt = require('bcryptjs');
 const User = require('../../models/User')
 
 module.exports = {
   Mutation: {
-    register(parent, { registerInput: {username, email, password} }, context, info) {
+    async register(parent, { registerInput: {username, email, password} }, context, info) {
       //validate user data
+      let hash = bcrypt.hashSync(password, 8);
+      console.log('hash', hash);
       const newUser = new User({
-          username, email, password, createdAt: "1200-23-232"
-      })
-      newUser.save();
+          username, email, password: hash, createdAt: "1200-23-232"
+      });
+      const res =  await newUser.save();
+      return { ...res._doc, id: res._id, token : 'this is gonna be a token in a future' }
     },
   },
 };
